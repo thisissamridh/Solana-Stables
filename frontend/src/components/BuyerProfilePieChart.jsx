db3172b64a52160b0e333e091531a1a3205fc5ac
Abhinaw Ratan <75108723+AbhinawRatan@users.noreply.github.com>
2023-07-24 17:27:58 +0530

    linegraph

--- frontend/src/components/BuyerProfilePieChart.jsx
+++ frontend/src/components/BuyerProfilePieChart.jsx
@@ -25,7 +25,7 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 export default function BuyerProfilePieChart() {
 	return (
 		<div className="w-[20rem] h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col">
-			<strong className="text-gray-700 font-medium">Buyer Profile</strong>
+			<strong className="text-gray-700 font-medium">Top3 Stablecoin </strong>
 			<div className="mt-3 w-full flex-1 text-xs">
 				<ResponsiveContainer width="100%" height="100%">
 					<PieChart width={400} height={300}>
--- frontend/src/components/TransactionChart.jsx
+++ frontend/src/components/TransactionChart.jsx
@@ -1,96 +1,47 @@
 import React from 'react'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 const data = [
-	{
-		name: 'Jan',
-		Expense: 4000,
-		Income: 2400
-	},
-	{
-		name: 'Feb',
-		Expense: 3000,
-		Income: 1398
-	},
-	{
-		name: 'Mar',
-		Expense: 2000,
-		Income: 9800
-	},
-	{
-		name: 'Apr',
-		Expense: 2780,
-		Income: 3908
-	},
-	{
-		name: 'May',
-		Expense: 1890,
-		Income: 4800
-	},
-	{
-		name: 'Jun',
-		Expense: 2390,
-		Income: 3800
-	},
-	{
-		name: 'July',
-		Expense: 3490,
-		Income: 4300
-	},
-	{
-		name: 'Aug',
-		Expense: 2000,
-		Income: 9800
-	},
-	{
-		name: 'Sep',
-		Expense: 2780,
-		Income: 3908
-	},
-	{
-		name: 'Oct',
-		Expense: 1890,
-		Income: 4800
-	},
-	{
-		name: 'Nov',
-		Expense: 2390,
-		Income: 3800
-	},
-	{
-		name: 'Dec',
-		Expense: 3490,
-		Income: 4300
-	}
+    {
+        date: '2022-01-01',
+        USDT: 4000,
+        USDC: 2400,
+        DAI: 3000
+    },
+    {
+        date: '2022-02-01',
+        USDT: 3000,
+        USDC: 1398,
+        DAI: 2500
+    },
+    {
+        date: '2022-03-01',
+        USDT: 2000,
+        USDC: 9800,
+        DAI: 2000
+    },
+    // ...
 ]
 
-export default function TransactionChart() {
-	return (
-		<div className="h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
-			<strong className="text-gray-700 font-medium">Transactions</strong>
-			<div className="mt-3 w-full flex-1 text-xs">
-				<ResponsiveContainer width="100%" height="100%">
-					<BarChart
-						width={500}
-						height={300}
-						data={data}
-						margin={{
-							top: 20,
-							right: 10,
-							left: -10,
-							bottom: 0
-						}}
-					>
-						<CartesianGrid strokeDasharray="3 3 0 0" vertical={false} />
-						<XAxis dataKey="name" />
-						<YAxis />
-						<Tooltip />
-						<Legend />
-						<Bar dataKey="Income" fill="#0ea5e9" />
-						<Bar dataKey="Expense" fill="#ea580c" />
-					</BarChart>
-				</ResponsiveContainer>
-			</div>
-		</div>
-	)
+export default function StablecoinChart() {
+    return (
+        <div className="h-[22rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
+            <strong className="text-gray-700 font-medium">Stablecoin Market Cap</strong>
+            <div className="mt-3 w-full flex-1 text-xs">
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart width={500} height={300} data={data}
+                        margin={{ top: 5, right: 30, left: -10, bottom: 5 }}>
+                        <XAxis dataKey="date" />
+                        <YAxis />
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="USDT" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="USDC" stroke="#82ca9d" />
+                        <Line type="monotone" dataKey="DAI" stroke="#ffc658" />
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+        </div>
+    )
 }
