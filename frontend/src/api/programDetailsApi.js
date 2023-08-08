0513da233edb39574181d79129ca02e357e97d85
thisissamridh <samridhsinghind@gmail.com>
2023-08-08 10:51:58 +0530

    polyfills

--- frontend/src/api/programDetailsApi.js
+++ frontend/src/api/programDetailsApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://public-api.solscan.io/account/';
-
+console.log(process.env.REACT_APP_SCAN);
 export const fetchProgramDetails = async (address) => {
     try {
         const response = await axios.get(`${BASE_URL}${address}`, {
             headers: {
-                'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOjE2ODgyMjE5NDU5NDMsImVtYWlsIjoic2FtcmlkaHNpbmdoaW5kQGdtYWlsLmNvbSIsImFjdGlvbiI6InRva2VuLWFwaSIsImlhdCI6MTY4ODIyMTk0NX0.tWGXAKiFSQQlm2xRi_0WZOwjGqFQ49j678cWzWgV37Y'
+                'token': `${process.env.REACT_APP_SCAN}`
             }
         });
         return response.data;
